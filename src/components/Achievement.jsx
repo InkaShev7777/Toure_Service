1ffb8baa46149536326e8bb473844348acd4d27e
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-04-07 18:33:19 +0300

    add achivements add desc for it

---
+++ src/components/Achievement.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const Achievement = () => {
+  return (
+    <>
+      <div className="achivements">
+        <div className="wrapper-full">
+          <div className="achivements__achievement achievement">
+            <div className="achievemt__pic">
+              <img
+                src="http://centr-tur.com/uploads/posts/2014-11/1415107128_img_0413_1.jpg"
+                alt="some img"
+                className="achivement__thumb"
+              />
+            </div>
+            <div className="achivement__info">
+              <h3 className="achivement__title">НАГОРОДИ</h3>
+              <p className="achivement__desc">
+                Нагороди – це визнання досягнень та заслуг. Вони мотивують людей
+                до нових висот, а також слугують символом пошани та вдячності.
+                Існують різні типи нагород: державні, міжнародні, галузеві,
+                громадські. Їх вручають за видатні успіхи в науці, мистецтві,
+                спорті, бізнесі, волонтерській діяльності та інших сферах.
+              </p>
+              <a href="#!" className="achivement__more">
+                ДЕТАЛЬНІШЕ
+              </a>
+            </div>
+          </div>
+
+          <div className="achivements__achievement achievement">
+            <div className="achivement__info">
+              <h3 className="achivement__title">НАВЧАЛЬНО-ПРАКТИЧНІ ЦЕНТРИ</h3>
+              <p className="achivement__desc">
+                Під девізом «Якісно! Професійно! Гостинно!» працюють
+                навчально-практичні центри, для яких підготовка
+                конкурентоспроможних кадрів стала закономірним наслідком
+                вкладання сил, натхнення і душі педагогічного колективу
+                Дніпровського центру професійно-технічної освіти туристичного
+                сервісу.
+              </p>
+              <a href="#!" className="achivement__more">
+                ДЕТАЛЬНІШЕ
+              </a>
+            </div>
+            <div className="achievemt__pic">
+              <img
+                src="http://centr-tur.com/uploads/posts/2015-08/1441007982_npc.jpg"
+                alt="some img"
+                className="achivement__thumb"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import '../styles/App.css';
 import {Header} from '../components/Header'
 import {News} from '../components/News'
 import {Places} from '../components/Places'
+import {Achievement} from '../components/Achievement'
 function App() {
   return (
     <div className="App">
       <Header/>
       <News/>
       <Places/>
+      <Achievement/>
     </div>
   );
 }
