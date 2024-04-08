b18a7cb0bdce2b546dfec2779f3d27facb4ed4f4
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-04-08 16:26:25 +0300

    add burge(menu) and action click button for hide menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,36 @@ import "../styles/App.css";
 import React, { useState, useEffect } from 'react';
 
 export const Header = () => {
-  
-    const [isActive, setIsActive] = useState(false);
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollY = window.scrollY;
-        setIsActive(scrollY > 50);
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+  const [isActive, setIsActive] = useState(false);
+  const [isActiveBurger, setIsActiveBurger] = useState(false);
 
-    return (
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setIsActive(scrollY > 50);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+
+    const burger = document.querySelector('.burger');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+
+    burger.addEventListener('click', () => {
+      setIsActiveBurger(!isActiveBurger);
+    });
+
+    menuCloseItem.addEventListener('click', () => {
+      setIsActiveBurger(!isActiveBurger);
+    });
+  }, [isActiveBurger]);
+
+  return (
     <>
       <header className={isActive ? 'header header_active' : 'header'}>
         <div className="wrapper">
@@ -30,7 +45,7 @@ export const Header = () => {
                 ></img>
               </a>
             </div>
-            <nav className="header__nav">
+            <nav className={isActiveBurger ? 'header__nav header__nav_active' : 'header__nav'}>
               <ul className="header__list">
                 <li className="header__item">
                   <a href="#!" className="header__link">Про нас</a>
@@ -54,16 +69,25 @@ export const Header = () => {
                   <a href="#!" className="header__link">Профорієнтація</a>
                 </li>
               </ul>
+              <div className="header__nav-close">
+                <span className="header__nav-close-line"></span>
+                <span className="header__nav-close-line"></span>
+              </div>
             </nav>
+            <div className="header__burger burger">
+              <span className="burger__line burger__line_first"></span>
+              <span className="burger__line burger__line_second"></span>
+              <span className="burger__line burger__line_third"></span>
+            </div>
           </div>
         </div>
       </header>
       <main className="main">
         <section className="intro">
-            <div className="wraper">
-                <h1 className="intro__title">ДНІПРОВСЬКИЙ ЦЕНТР ПРОФЕСІЙНО-ТЕХНІЧНОЇ ОСВІТИ ТУРИСТИЧНОГО СЕРВІСУ</h1>
-                <p className="intro__subtitle">Державний професійно-технічний навчальний заклад</p>
-            </div>
+          <div className="wraper">
+            <h1 className="intro__title">ДНІПРОВСЬКИЙ ЦЕНТР ПРОФЕСІЙНО-ТЕХНІЧНОЇ ОСВІТИ ТУРИСТИЧНОГО СЕРВІСУ</h1>
+            <p className="intro__subtitle">Державний професійно-технічний навчальний заклад</p>
+          </div>
         </section>
       </main>
     </>
