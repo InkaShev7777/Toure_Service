5875923bdfcf71ae427b20536fa768dd0957db9a
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-04-09 14:15:34 +0300

    add preloader for

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Preloader = () => {
+    return (
+        <>
+            <div className='loader'>
+                <h1 className='loader__title'>Loading</h1>
+            </div>
+        </>
+
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,32 +6,44 @@ import { Achievement } from '../components/Achievement'
 import { News } from '../components/News'
 import { Staff } from '../components/Staff'
 import { Footer } from '../components/Footer'
+import { Preloader } from '../components/Preloader';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(true);
+    }, 1200);
+  }, []);
 
   return (
-    <div className="App">
-      <Header />
-      <div>
-        <div id='mainInfo'>
-          <MainInfo />
+    <>
+      {isLoading ? <div className="App">
+        <Header />
+        <div>
+          <div id='mainInfo'>
+            <MainInfo />
+          </div>
+          <div id='places'>
+            <Places />
+          </div>
+          <div id='achievement'>
+            <Achievement />
+          </div>
+          <div id="_news">
+            <News />
+          </div>
+          <div id='staff'>
+            <Staff />
+          </div>
         </div>
-        <div id='places'>
-          <Places />
-        </div>
-        <div id='achievement'>
-          <Achievement />
-        </div>
-        <div id="_news">
-          <News />
-        </div>
-        <div id='staff'>
-          <Staff />
-        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+  : <Preloader/>}
+    </>
   );
 }
 export default App;
