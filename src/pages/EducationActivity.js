fe8bb27bb0953ab2f10b7b0075734d982163c234
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-04-27 15:45:35 +0300

    update text for news, mainInfo. Add page for ResActiv, ScuenMetWork,

--- src/pages/EducationActivity.js
+++ src/pages/EducationActivity.js
@@ -1,11 +1,197 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 export const EducationActivity = () => {
     return (
         <div>
             <Header />
-            <h1>EducationActivity</h1>
+            <section className="benefits">
+                <div className="benefits__wrap">
+                    <h2 className="benefits__title">Навчальна діяльність</h2>
+                    <section className="news">
+                        <div className="wrapper-full">
+                            <div className="news__cards">
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/C94c5gT/IMG-20220622-200428-004.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                ДКА в гр.Ф-21-1
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        21 червня 2022 року відбувся захист групи флористів на присвоєння кваліфікаційного 5 розряду згідно нового Державного стандарту. Це перший випуск з присвоєнням такого розряду. Вітаємо нових кадрів, які будуть розбудовувати нашу Україну і робити її ще гарнішою!
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://m.facebook.com/story.php?story_fbid=pfbid021ReB8uCHJx6ZqT9BjN6v6NPJeaD6mFMjmipznuehYVFv8qfRyGtMQw82y978uPz4l&id=100055532898506" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/5M0BHDB/IMG-20221112-115047-187.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                Проміжна атестація в гр.КК-20-22
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        10.11.22 в групі КК-20-22 відбулась проміжна кваліфікаційна атестація. Учні продемонстрували свої знання та навички, здобуті під час навчання та отримали кваліфікаційний розряд “Кухар 3 розряду”.
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/R2qCmvL/IMG-20220930-113337-030.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                Проміжна атестація в гр.ПМП-19-16
+                                            </h3>
+
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        Проміжна атестація відбулась 28.09.2022 у гр.ПМП-19-16. Учениці успішно виконали екзаменаційні роботи та отримали кваліфікацію “Манікюрник 2 розряду”, з чим їх щиро вітаємо!
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/FXV77Vg/IMG-20220520-220749-370.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                Проміжна атестація в гр. ПМП-21-8
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        20 травня 2022 року в гр.ПМП-21-8 відбулася проміжна атестація, на якій учні успішно захистили свої роботи з виконання чоловічих та жіночих стрижок, укладки і фарбування волосся та отримали кваліфікацію “Перукар 2 класу”, з чим їх хочемо привітати!
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/wM54wB4/IMG-96b4dc07acfd64496573866b9d341e7d-V.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                ДКА в гр.КК-18-14
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        21.12.2021 в групі КК-18-14 відбулась державна кваліфікаційна атестація на присвоєння робітничого розряду “Кондитер 3 розряду”. Спочатку учні виконували письмову частину, а потім захищали свої практичні роботи – різноманітні кондитерські вироби. Всі учні відповідально й творчо підійшли до проходження атестації, адже попереду – доросле життя вже кваліфікованого робітника, що має дуже затребувану професію “Кухар. Кондитер”! Тож вітаємо їх з успішним завершенням ще однієї сходинки свого життя!
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/5Bytdns/IMG-736a75069fbf15e4f2ced676c996c7fc-V.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                Проміжна атестація в групі ПМП-21-25
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        Сьогодні в групі ПМП-21-25 відбулась проміжна державна кваліфікаційна атестація на захист кваліфікації “Перукар”. Учні виконували чоловічу, жіночу класичні стрижки та фарбування “Тон в тон”.
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/MkzPLd5/F2-C0-E0-EC-33-BD-4-A8-C-BDDA-6-B7-D3513-F7-AB.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                ДКА в гр.ПМП-19-16
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        21.12.2022 відбулася державна кваліфікаційна атестація з професії «Перукар. Манікюрник. Педикюрник» в групі ПМП-19-16.
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/C9mp3h8/20210225-184112.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                Проміжна атестація в гр. КК-18-14
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        24 лютого 2021 року в групі КК-18-14 відбулася проміжна атестація на здобуття кваліфікації “Кухар 4 розряду”. Здобувачі освіти продемонстрували свої вміння на різних видах супу, готували лосося, медальйони, шніцелі, салати і десерти, деякі учні приємно вразили членів комісії ресторанною подачею страв. Майже всі учні отримали оцінки достатнього і високого рівня! З чим їх і вітаємо!
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+
+
+                                <div className="news__card">
+                                    <div className="news__card-pic">
+                                        <img
+                                            className="news__card-thumb"
+                                            src="https://i.ibb.co/zFWjwnR/IMG-4a636f0d8fad49a2b213b606b6e688f8-V.jpg"
+                                            alt="img"
+                                        />
+                                        <div className="news__card-stats">
+                                            <h3 className="news__card-title">
+                                                Проміжна атестація в гр. КМ-20-20
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <p className="news__card-desc">
+                                        17 червня 2021 року у Центрі проходила проміжна кваліфікаційна атестація на здобуття професії “Кухар” 3 розряду. Здобувачі освіти групи КМ-20-20 готували страви відповідно до стандарту та технологічних карт, а комісія мала змогу їх продегустувати та оцінити вміння та навички, здобуті під час навчання професії.
+                                    </p>
+                                    <a style={{ width: '160px' }} target='blank' href="https://www.facebook.com/dcptots" className="news__card-more">БІЛЬШЕ ФОТО</a>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </section>
+            <Footer />
         </div>
     )
 }
--- src/pages/ScientificMethodicalWork.js
+++ src/pages/ScientificMethodicalWork.js
@@ -1,11 +1,199 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 export const ScientificMethodicalWork = () => {
   return (
     <div>
-        <Header/>
-        <h1>ScientificMethodicalWork</h1> 
+      <Header />
+      <section className="benefits">
+        <div className="benefits__wrap">
+          <h2 className="benefits__title">Науково-методична робота</h2>
+          <iframe src="https://www.symbaloo.com/embed/web-%D1%81%D0%B5%D1%80%D0%B2%D1%96%D1%81%D0%B8%D0%B4%D0%BB%D1%8F%D0%BF%D0%B5%D0%B4%D0%B0%D0%B3%D0%BE%D0%B3%D1%96%D0%B2?" name="_symFrame" width="100%" style={{ padding: '20px' }} height="600px" frameborder="0"></iframe>
+          <div className='wrapper'>
+            <h3 className='news__card-desc'>Віртуальні дошки:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.educreations.com/'>EDUCREATIONS</a> – віртуальна дошка, класи, категорії, мультимедіа
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://popplet.com/'>POPPLET</a> – віртуальна стіна для роботи з мультимедіа об’єктами в групі
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://scrumblr.ca/'>SCRUMBL</a> –віртуальна дошка зі стікерами для групової роботи
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.twiddla.com/'>TWIDDLA</a> – віртуальна інтерактивна дошка для спільної роботи
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Офісні сервіси:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://apps.google.com/intx/ru/products/docs/?utm_source=google&utm_medium=cpc&utm_campaign=emea-ua-ru-docs-bkws-all-trial-e_c&utm_term=google%20%D0%B4%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B&KWID=43700009068573780'>GOOGLE Документи</a> – онлайн створення текстових файлів, таблиць, презентацій
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://textcoworker.ru/'>TEXTCOWORKER</a> – сервіс для спільної роботи над документами
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Графіка:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='http://anymaking.com/fun-photo-effects'>ANYMAKING</a> – прості інструменти для редагування фото, використання різних фотоефектів
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://avatan.ru/'>AVATAN</a> – різні ефекти для фото, створення фотоколажів
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://burner.bonanza.com/'>BACKGROUND BURNER</a> – сервіс для видалення фону з фотографії
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.befunky.com/'>BEFUNKY</a> – редагування фото, створення фотоколажів
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://calendarum.ru/'>CALENDARUM</a> – зручний, швидкий сервіс для створення візиток, календарів, конвертів
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.canva.com/'>CANVA</a> – сервіс для створення постерів, банерів, візиток
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://clippingmagic.com/'>CLIPPINGMAGIC</a> – видалення фону з фото
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.glogster.com/'>GLOGSTER</a> – створення мультимедіа постерів (глогів)
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.thinglink.com/'>THINGLINK</a> – створення інтерактивних зображень, плакатів (групова робота)
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Презентації, публікації:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.emaze.com/ru/'>EMAZE</a> – створення презентацій з захоплюючим дизайном та тривимірною анімацією
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://fliphtml5.com/'>FLIPHTML5</a> – сервіс інтерактивних електронних публікацій
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.peardeck.com/'>PEAR DECK</a> – додаток для інтерактивних презентацій
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://prezi.com/'>PREZI</a> – створення «літаючих» презентацій
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.sparkol.com/'>SPARKOL</a> – скрайб-презентації
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://en.yumpu.com/'>YUMPU</a> – онлайн фліпбук
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.zyyne.com/site/'>ZYYNE</a> – електронні публікації у форматі html
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Візуалізація:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://piktochart.com/'>PIKTOCHART</a> – створення інфографіки
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.zooburst.com/'>ZOOBURST</a> – анімаційні 3d публікації у вигляді історій, казок
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Дидактичні ігри:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.classtools.net/'>CLASSTOOLS</a> – багатоінструментальний сервіс для дидактичних ігор
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.jigsawplanet.com/'>JIGSAW PLANET</a> – створення паз злів
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://learningapps.org/'>LEARNINGAPPS</a> – створення інтерактивних навчальних вправ
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.triventy.com/'>TRIVENTY</a> – колаборативні вікторини
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Стрічки часу:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.tiki-toki.com/'>TIKI-TOKI</a> – створення інтерактивних 3d хронологічних стрічок
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Карти:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='http://www.tripline.net/'>TRIPLINE</a> – створення інтерактивних карт-ілюстрацій для подорожей
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Відеосервіси:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.youtube.com/'>YOUTUBE</a> – популярний відеохостинг з можливістю редагування відео, додавання інтерактивного контенту
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Тести, опитувальники:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='http://master-test.net/uk'>MASTER TEST</a> – створення тестів, проведення тестування онлайн
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://quizlet.com/'>QUIZLET</a> – простий сервіс для створення тестів онлайн
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Вебінари:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.anymeeting.com/'>ANYMEETING</a> – система організації вебінарів, відеоконференцій (до 200 учасників)
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://pruffme.com/'>PRUFFME</a> – платформа для вебінарів
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Сайти:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.anymeeting.com/'>GOOGLE SITE</a> – створення сайту
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://pruffme.com/'>WIX</a> – створення і супровід flash-сайту
+              </li>
+            </ul>
+
+            <h3 className='news__card-desc'>Безкоштовні дистанційні курси:</h3>
+            <ul>
+              <li>
+                <a style={{ color: 'black' }} href='https://sites.google.com/site/edugservis/home'>«Сервіси Google у професійній діяльності викладача»</a>
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://dystosvita.mdl2.com/'>«Дистанційне навчання інформатики»</a>
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://onlainnavchanna.blogspot.com/'>Cтудія онлайн навчання для вчителів</a>
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='http://interactive.ranok.com.ua/course/group/distantsyin-kyrsi-dlya-vchitelv'>Iнтерактивне навчання для педагогів</a>
+              </li>
+              <li>
+                <a style={{ color: 'black' }} href='https://www.coursera.org/'>Oнлайн платформа масових відкритих онлайн курсів</a>
+              </li>
+            </ul>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+      </section>
+      <Footer/>
     </div>
   )
 }
